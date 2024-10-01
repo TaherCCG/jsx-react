04584b8246b07b731dc1658ab36f60e29159bcfa
Taher Mahmood <taher.ccg@gmail.com>
2024-10-01 17:29:04 +0000

    Add: Statful With Callback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <FunctionalGreeting/>
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="T" age="46" />
       <StatefulGreeting greeting="Im Stateful class!" />
+      <StatefulGreetingWithCallback greeting="Im Stateful class With Callback!" />
+
     </div>
   );
 }
