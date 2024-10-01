fbade0a04202a6106ad10a1dea0264aab1ca9bfe
Taher Mahmood <taher.ccg@gmail.com>
2024-10-01 10:56:09 +0000

    Add:StatefulGreeting component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreeting greeting="Im Stateful prop!" />
       <FunctionalGreeting/>
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="T" age="46" />
     </div>
