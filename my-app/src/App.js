96f2efb6425971c012b05f6ee59c6aa364eb5ad9
Taher Mahmood <taher.ccg@gmail.com>
2024-10-02 14:53:49 +0000

    Methods as Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,11 +12,12 @@ import './App.css';
 // import EventBinding from "./components/EventBinding";
 // import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 // import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
-import NestingComponents from "./components/NestingComponents";
+// import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return <div className="App">
-      <NestingComponents />
+      <MethodsAsPropsParent />
   </div>;
 }
 
